acb3d5b6220f90140a478ff1747d9f22deab8113
Karen Valadez <valadez.karen04@gmail.com>
2021-05-26 12:15:46 -0500

    first one solved

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,5 +1,8 @@
 public class StringExercise {
     public static void main(String[] args) {
-
+        String noEducation = "We don't need no education";
+        System.out.println(noEducation);
+        noEducation = noEducation.replace("education", "thought control");
+        System.out.println(noEducation);
     }
 }
