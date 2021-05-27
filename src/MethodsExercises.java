b7b82b1b78de740d51f3a0b3bd4724305a4a82f9
Karen Valadez <valadez.karen04@gmail.com>
2021-05-27 11:56:17 -0500

    exercise class created

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,8 +1,16 @@
-
+import java.util.Scanner;
 
 public class MethodsExercises {
 
+
+
     public static void main(String[] args) {
+
+        System.out.println(addIt(14, 14));
+        System.out.println(subtractIt(100, 50));
+        System.out.println(multiplyIt(21, 50));
+        System.out.println(devideIt(2000, 50));
+
 
     }
 }
