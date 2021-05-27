bddbb7d9f3621cc030b45bfefeaa9b4827b943d4
Karen Valadez <valadez.karen04@gmail.com>
2021-05-27 09:19:29 -0500

    exercise and walkthrough completed

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,8 +1,19 @@
+
+
 public class StringExercise {
     public static void main(String[] args) {
-        String noEducation = "We don't need no education";
+        String noEducation = "We don't need no education\nWe don't need no thought control";
         System.out.println(noEducation);
-        noEducation = noEducation.replace("education", "thought control");
-        System.out.println(noEducation);
+
+        String quotes = "Check \"this\" out!, \"s inside of \"s";
+        System.out.println(quotes);
+
+        String windowDrive = "In windows, the main drive is usually C:\\";
+        System.out.println(windowDrive);
+
+
+        String slashes = "I can do backslashes \\, double backslashes \\\\,\n" +
+                "and the amazing triple backslash \\\\\\!";
+        System.out.println(slashes);
     }
 }
