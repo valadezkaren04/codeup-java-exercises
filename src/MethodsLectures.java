14b9bfb9fe095bfa6c845c96c03a44c45d247938
Karen Valadez <valadez.karen04@gmail.com>
2021-05-28 22:00:08 -0500

    updated notes

--- src/MethodsLectures.java
+++ src/MethodsLectures.java
@@ -44,19 +44,3 @@ public class MethodsLectures {
     }
 
 
-    public static void main(String[] args) {
-        sayHello("Karen"); //Hi there, my name is Karen! Have a great day!
-        sayHello("Heeyy", "karen"); //Heeyy, my name is karen! Have a great day!
-        sayHello(4); //consoles 4 of them
-
-        System.out.println(multiply(5,2)); //10
-        int product = multiply(14,24); // 336
-        System.out.printf("Did you know that %d * %d = %d?", product, product, product * product); //Did you know that 336 * 336 = 112896?
-
-        Scanner sc = new Scanner(System.in);
-        willYouTakeUsToMtSplashmore(sc);
-
-        bottlesOfBlank("Surge", 15);
-
-    }
-}
