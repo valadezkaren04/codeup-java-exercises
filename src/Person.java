0f668d6f5aeb457d0a077a9216c5161613c925dd
Karen Valadez <valadez.karen04@gmail.com>
2021-06-01 15:55:37 -0500

    first exercise complete...I think ; double check later

--- src/Person.java
+++ src/Person.java
@@ -1,13 +1,19 @@
 public class Person {
+
     private String name;
+
     public String getName(){
 //TODO: return the person's name
+        return String.format(name);
     }
 
     public void setName(String name){
 //TODO: change the name property to the passed value
+        this.name = name;
     }
+
     public void sayHello(){
 //TODO: print a message to the console using the person's name
+        String.format("Hello, %s", this.name);
     }
 }
