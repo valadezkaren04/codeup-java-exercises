d971524e0ccfd99dac89d21c350a320842691953
Karen Valadez <valadez.karen04@gmail.com>
2021-06-02 10:26:41 -0500

    halway through validation class exercise

--- src/util/Input.java
+++ src/util/Input.java
@@ -1,11 +1,36 @@
 package util;
+import java.util.Locale;
+import java.util.Scanner;
+
 
 public class Input {
-    private String scanner;
+    private Scanner scanner;
 
-//    String getString()
-//    boolean yesNo()
-//    int getInt(int min, int max)
+    public Input (){
+        this.scanner = new Scanner(System.in); //created a constructor.
+
+    }
+
+    public String getString(){ //getter
+        System.out.println("Please enter a sentence");
+        String input = scanner.nextLine();
+        return input;
+    }
+
+    public boolean YesNo(){
+        System.out.println("Yes or No?");
+        String input = scanner.nextLine();
+        if(input.equalsIgnoreCase("yes")||input.equalsIgnoreCase("y")){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public int getInt(int min, int max) {
+
+    }
+
 //    int getInt()
 //    double getDouble(double min, double max)
 //    double getDouble()
