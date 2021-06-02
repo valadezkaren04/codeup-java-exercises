8dfe646fa7645f9f92f116c1e57c3bc5209c7136
Karen Valadez <valadez.karen04@gmail.com>
2021-06-02 10:14:42 -0500

    not entirely complete

--- src/util/Input.java
+++ src/util/Input.java
@@ -1,4 +1,13 @@
 package util;
 
 public class Input {
+    private String scanner;
+
+//    String getString()
+//    boolean yesNo()
+//    int getInt(int min, int max)
+//    int getInt()
+//    double getDouble(double min, double max)
+//    double getDouble()
+
 }
