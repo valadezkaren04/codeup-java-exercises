f8835d826bb4944481f3077ca9d14c32bbf52dde
Karen Valadez <valadez.karen04@gmail.com>
2021-06-02 12:02:39 -0500

    fixed mistakes

--- src/util/Input.java
+++ src/util/Input.java
@@ -1,41 +1,65 @@
 package util;
-import java.util.Locale;
+//import java.util.Locale;
 import java.util.Scanner;
 
 
 public class Input {
     private Scanner scanner;
 
-    public Input (){
-        this.scanner = new Scanner(System.in); //created a constructor (special method that is called when an object is created)
-
+    public Input () {
+        this.scanner = new Scanner(System.in); // When an instance of this object is created, the scanner property should be set to a new instance of the Scanner class. ; created a CONSTRUCTOR (special method that is called when an object is created).
     }
 
     public String getString(){ //getter
-        System.out.println("Please enter a sentence");
-        String input = scanner.nextLine();
-        return input;
+        System.out.println("Please enter a sentence...");
+        return this.scanner.nextLine(); // can be in one line instead
+        //String input = scanner.nextLine();
+        // return input;
     }
 
-    public boolean YesNo(){
+    public boolean yesNo() {
         System.out.println("Yes or No?");
-        String input = scanner.nextLine();
-        if (input.equalsIgnoreCase("yes")||input.equalsIgnoreCase("y")){
+        String Userinput = scanner.nextLine();
+        if (Userinput.trim().equalsIgnoreCase("yes")||Userinput.trim().equalsIgnoreCase("y")) {
             return true;
         } else {
             return false;
         }
     }
 
-    public int getInt(int min, int max) {
-        System.out.println("Please enter an integer between 1-20: ");
-        String input = scanner.nextLine();
-        if ()
-
+    public int getInt() {
+        return this.scanner.nextInt();
     }
 
-//    int getInt()
-//    double getDouble(double min, double max)
+    public int getInt(int min, int max) {
+        System.out.printf("Please enter an integer between %d and %d", min, max);
+        int userNum = this.scanner.nextInt();
+
+        if (userNum >= min && userNum <= max) {
+            return userNum;
+        } else {
+            System.out.println("That number is not between the max and min");
+            return getInt(min, max);
+        }
+    }
+
+    public double getDouble(double min, double max) {
+        System.out.printf("Give me a decimal between %f and %f", min, max);
+        double userNum = this.scanner.nextDouble();
+
+        if (userNum >= min && userNum <= max) {
+            return userNum;
+        } else {
+            System.out.println("That decimal number is not between the max and min");
+            return getDouble(min, max);
+        }
+    }
+
+    public double getDouble() {
+        return this.scanner.nextInt();
+    }
+
+
 //    double getDouble()
 
 }
