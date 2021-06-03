450bf6869abb15ea1f8218645fb8db8babbcf000
Karen Valadez <valadez.karen04@gmail.com>
2021-06-03 10:39:17 -0500

    Starting on exercise

--- src/ArrayLecNotes.java
+++ src/ArrayLecNotes.java
@@ -1,4 +1,3 @@
-
 import java.util.Arrays;
 public class ArrayLecNotes {
     public static void main(String[] args) {
---
+++ src/ArraysExercises.java
@@ -0,0 +1,9 @@
+public class ArraysExercises {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        System.out.println(numbers); // [I@5acf9800 ; prints location of array in memory - not human readable.
+
+
+
+    }
+}
