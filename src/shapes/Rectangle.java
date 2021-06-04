359a98392e4d0756442094651d1f0babb7e95795
Karen Valadez <valadez.karen04@gmail.com>
2021-06-04 11:12:49 -0500

    exercise not complete

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,4 +1,17 @@
 package shapes;
 
 public class Rectangle {
+
+    //it should have protected properties for both length and width.
+    protected double length;
+    protected double width;
+
+    //Rectangle should define a constructor that accepts two numbers for length and width, and sets
+    //those properties.
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public double getArea;
 }
