556ad39e30e79ff5a0d0510695e01b1a4cf39991
Karen Valadez <valadez.karen04@gmail.com>
2021-06-04 15:30:19 -0500

    lecture notes added; not complete

---
+++ src/KitchenApp/Appliance.java
@@ -0,0 +1,8 @@
+package KitchenApp;
+
+public abstract class Appliance {
+    public static void main(String[] args) {
+        Appliance app1 = new Appliance() {
+        }
+    }
+}
---
+++ src/KitchenApp/ApplianceTest.java
@@ -0,0 +1,6 @@
+package KitchenApp;
+
+//Abstract class
+// Conserve as a super class, but abstract class we can't directly instantiate an abstract class
+public class ApplianceTest {
+}
