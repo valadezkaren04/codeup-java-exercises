d1871427e9cefaed920367804066ca28ef06246b
Karen Valadez <valadez.karen04@gmail.com>
2021-06-04 13:30:52 -0500

    commit

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -6,12 +6,33 @@ public class Rectangle {
     protected double length;
     protected double width;
 
-    //Rectangle should define a constructor that accepts two numbers for length and width, and sets
-    //those properties.
-    public Rectangle(double length, double width) {
+    // Getters and Setters
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
         this.length = length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
         this.width = width;
     }
 
-    public double getArea;
+    //Rectangle should define a constructor that accepts two numbers for length and width, and sets
+    //those properties ; constructor
+    public Rectangle(double length, double width) {
+        this.setLength(length);
+        this.setWidth(width);
+    }
+
+    public double getArea() {
+        return this.getLength() * this.getWidth();
+    }
+
+
 }
