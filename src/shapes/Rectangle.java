446f4dc9113846380dc04c8e7e1a59e841cd9520
Karen Valadez <valadez.karen04@gmail.com>
2021-06-05 08:15:14 -0500

    broken code

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -2,46 +2,46 @@ package shapes;
 
 public class Rectangle {
 
-    //it should have protected properties for both length and width.
-    protected double length;
-    protected double width;
-
-    // Getters and Setters
-    public double getLength() {
-        return length;
-    }
-
-    public void setLength(double length) {
-        this.length = length;
-    }
-
-    public double getWidth() {
-        return width;
-    }
-
-    public void setWidth(double width) {
-        this.width = width;
-    }
-
-    // default constructor
-    public Rectangle() {
-
-    }
-
-    //Rectangle should define a constructor that accepts two numbers for length and width, and sets
-    //those properties ; constructor
-    public Rectangle(double length, double width) {
-        this.setLength(length);
-        this.setWidth(width);
-    }
-
-    public double getArea() {
-        return this.getLength() * this.getWidth();
-    }
-
-    public double getPerimeter() {
-        return (2 * this.getLength() + (2 * this.getWidth()));
-    }
+//    //it should have protected properties for both length and width.
+//    protected double length;
+//    protected double width;
+//
+//    // Getters and Setters
+//    public double getLength() {
+//        return length;
+//    }
+//
+//    public void setLength(double length) {
+//        this.length = length;
+//    }
+//
+//    public double getWidth() {
+//        return width;
+//    }
+//
+//    public void setWidth(double width) {
+//        this.width = width;
+//    }
+//
+//    // default constructor
+//    public Rectangle() {
+//
+//    }
+//
+//    //Rectangle should define a constructor that accepts two numbers for length and width, and sets
+//    //those properties ; constructor
+//    public Rectangle(double length, double width) {
+//        this.setLength(length);
+//        this.setWidth(width);
+//    }
+//
+//    public double getArea() {
+//        return this.getLength() * this.getWidth();
+//    }
+//
+//    public double getPerimeter() {
+//        return (2 * this.getLength() + (2 * this.getWidth()));
+//    }
 
 
 }
