ffb491a4eea214446aedb8a9027fa19abe71c7ab
Karen Valadez <valadez.karen04@gmail.com>
2021-06-09 09:04:58 -0500

    lecture notes

---
+++ src/ExceptionStudy.java
@@ -0,0 +1,44 @@
+//import java.util.Scanner;
+//
+//public class ExceptionStudy {
+//    public String maybeAnInteger = "42";
+//    public String definitelyNotAnInteger = "Nope. I'm not an integer, I'm a String.";
+//
+//    public void askForAnInterger() {
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("please enter an integer...");
+//        String userInput = scanner.nextLine();
+//        try {
+//            int userInputInt = Integer.parseInt(userInput);
+//            System.out.println("The integer in your string is: " + userInputInt);
+//        } catch (NumberFormatException e) {
+//            System.out.println("Oh nooo! " + e.getMessage() + " is not an integer");
+//        } finally {
+//            System.out.println("Thank you for using the app");
+//            System.out.println("Have a great day! n.n ");
+//        }
+//    }
+//
+//    // series of catches
+//    /**make sure you put more specific exceptions catches ABOVE more general exception catches
+//     * in your code or you will get thee already caught exception.
+//     */
+//    public void moreComplexInt() {
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("please enter an integer...");
+//        String userInput = scanner.nextLine();
+//        try {
+//            //userInput = null // npe
+//            int userInputInteger = Integer.parseInt(userInput, 10, 15, 10);
+//            System.out.println("Thank you for entering: " + userInputInteger);
+//        } catch (NumberFormatException nfe){
+//            System.out.println("No integer between characters 10 and 15");
+//        } catch (IndexOutOfBoundsException iobe) {
+//            System.out.println("Your string wasn't long enough");
+//        } catch (NullPointerException npe) {
+//            System.out.println("You literally didn't hear anything");
+//        } finally {
+//            System.out.println("Thank you for visiting our application.");
+//        }
+//    }
+//}
---
+++ src/ExceptionStudyTest.java
@@ -0,0 +1,27 @@
+public class ExceptionStudyTest {
+    public static void main(String[] args) {
+        //ExceptionStudy study = new ExceptionStudy(); //extanciates the class
+//        System.out.println(study.definitelyNotAnInteger); //Nope. I'm not an integer, I'm a String.
+//        System.out.println(study.maybeAnInteger); // 42
+//        System.out.println(Integer.parseInt(study.definitelyNotAnInteger)); // exception
+//        System.out.println(Integer.parseInt(study.maybeAnInteger)); // exception
+
+        // tries out the errors ; risky because it has an exception. Catches it before it can fail
+//        try {
+//            ExceptionStudy study = new ExceptionStudy();
+//            System.out.println(Integer.parseInt(study.definitelyNotAnInteger)); // exception
+//            System.out.println(Integer.parseInt(study.maybeAnInteger)); // exception
+//        } catch (NumberFormatException e) {
+//            System.out.println("Woah! o.O, that is not an integer...");
+//            System.out.println("I got an exception: " + e); // returns back what the exception is
+//            e.printStackTrace(); // traces the exception
+//        }
+
+        //AskingForAnInteger method ; runs it
+        ExceptionStudy study = new ExceptionStudy();
+        study.askForAnInterger();
+
+        // runs the method
+        study.moreComplexInt();
+    }
+}
