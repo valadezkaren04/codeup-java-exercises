75d2ba9b01918f51a89c903af3f29fd422d6a1ed
Karen Valadez <valadez.karen04@gmail.com>
2021-06-09 09:05:38 -0500

    lecture notes

--- src/ExceptionStudy.java
+++ src/ExceptionStudy.java
@@ -1,24 +1,24 @@
-//import java.util.Scanner;
-//
-//public class ExceptionStudy {
-//    public String maybeAnInteger = "42";
-//    public String definitelyNotAnInteger = "Nope. I'm not an integer, I'm a String.";
-//
-//    public void askForAnInterger() {
-//        Scanner scanner = new Scanner(System.in);
-//        System.out.println("please enter an integer...");
-//        String userInput = scanner.nextLine();
-//        try {
-//            int userInputInt = Integer.parseInt(userInput);
-//            System.out.println("The integer in your string is: " + userInputInt);
-//        } catch (NumberFormatException e) {
-//            System.out.println("Oh nooo! " + e.getMessage() + " is not an integer");
-//        } finally {
-//            System.out.println("Thank you for using the app");
-//            System.out.println("Have a great day! n.n ");
-//        }
-//    }
-//
+import java.util.Scanner;
+
+public class ExceptionStudy {
+    public String maybeAnInteger = "42";
+    public String definitelyNotAnInteger = "Nope. I'm not an integer, I'm a String.";
+
+    public void askForAnInterger() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("please enter an integer...");
+        String userInput = scanner.nextLine();
+        try {
+            int userInputInt = Integer.parseInt(userInput);
+            System.out.println("The integer in your string is: " + userInputInt);
+        } catch (NumberFormatException e) {
+            System.out.println("Oh nooo! " + e.getMessage() + " is not an integer");
+        } finally {
+            System.out.println("Thank you for using the app");
+            System.out.println("Have a great day! n.n ");
+        }
+    }
+
 //    // series of catches
 //    /**make sure you put more specific exceptions catches ABOVE more general exception catches
 //     * in your code or you will get thee already caught exception.
