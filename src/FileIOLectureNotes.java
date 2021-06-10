8f302b73d564964743ef7d63205d907d0fbb463c
Karen Valadez <valadez.karen04@gmail.com>
2021-06-10 10:06:14 -0500

    unfinished lec notes

--- src/FileIOLectureNotes.java
+++ src/FileIOLectureNotes.java
@@ -3,6 +3,8 @@ import java.io.IOException;
 import java.nio.file.*;
 // series of method that has to do with input and output in the new io
 // * = wild card - everything in the file
+import java.util.Arrays;
+import java.util.List;
 
 public class FileIOLectureNotes {
     public static void main(String[] args) {
@@ -12,9 +14,47 @@ public class FileIOLectureNotes {
 //        System.out.println(path);
 //        System.out.println(path.toAbsolutePath()); // /Users/av/IdeaProjects/codeup-java-exercises/src ; gives you the the path (where it came from)
 
-        Path path = Paths.get("src", "FileIOLectureNotes.java");
-        System.out.println(path);
-        System.out.println(path.toAbsolutePath());
+//        Path path = Paths.get("src", "FileIOLectureNotes.java");
+//        System.out.println(path);
+//        System.out.println(path.toAbsolutePath());
+
+//        Path pathToSrc = Paths.get("src"); //represent a path obj that represents src
+//        System.out.println(pathToSrc);
+//        Path wontWort = Paths.get(pathToSrc, "FileIOLectureNotes.java"); // needs to be a string
+
+        // creates directory
+        Path toOurDataPlace = Paths.get("src/data");
+//        System.out.println(toOurDataPlace);
+
+        try {
+            if (Files.notExists(toOurDataPlace)) {
+                Files.createDirectories(toOurDataPlace);
+            } else {
+                System.out.println("The " + toOurDataPlace + " directory already exists.");
+            }
+            Files.createDirectories(toOurDataPlace);
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+
+        Path toOurDataFile = Paths.get(String.valueOf(toOurDataPlace));
+
+        try {
+            if (Files.notExists(toOurDataFile)) {
+                Files.createFile(toOurDataFile);
+            }
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+
+        // Write to a file
+        List<String> romanEmpresses = Arrays.asList("Livia", "Agrippina", "Messalina", "Julia Domna");
+
+        try {
+            Files.write(toOurDataFile, romanEmpresses);
+        } catch (IO)
+
+
 
     }
 }
