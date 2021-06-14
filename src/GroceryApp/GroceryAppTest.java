be529ed01dcdc1981cd79bd6c36010ea2879f74c
Karen Valadez <valadez.karen04@gmail.com>
2021-06-14 10:59:31 -0500

    commenting out due to erros

--- src/GroceryApp/GroceryAppTest.java
+++ src/GroceryApp/GroceryAppTest.java
@@ -20,39 +20,39 @@ public class GroceryAppTest extends Input {
         while (!list.containsKey(selectSection))
             selectSection
     }
-
-    public static void printList(HashMap<String, ArrayList<Groceries>> list) {
-        System.out.println("Your list: \n");
-        int allSections = 0;
-        for (HashMap.Entry<String, ArrayList<Groceries>> pair : list.entrySet()) {
-            if (!pair.getValue().isEmpty()) {
-                allSections++;
-                System.out.printf("%s \n", pair.getKey());
-                for (Groceries item ; pair.getValue()) {
-                    if (item.getQuantity() > 1) {
-                        System.out.printf(" %s, %d, $%.2f each, $%.2f total%n", item.getName(), item.getQuantity(), item.getPrice(), (item.getQuantity() * item.getPrice()));
-                    } else {
-                        System.out.printf(" %s. $%.2f%n", item.getName(), item.getPrice());
-                    }
-                }
-                System.out.println();
-            }
-        }
-        if (allSections == 0) {
-            System.out.println("\nList is empty. \n Please add items to your list");
-        }
-    }
-
-    public static void runTheApp(HashMap<String, ArrayList<Groceries>> list, Input in) {
-        System.out.println("Welcome to BT21 Market\n");
-        System.out.println("*************************");
-
-        boolean run = true;
-        while(run) {
-            System.out.println("Please make your selection");
-            System.out.println("1. Add new item to the list");
-            System.out.println("2. View list");
-            System.out.println("3. Exit");
+//
+//    public static void printList(HashMap<String, ArrayList<Groceries>> list) {
+//        System.out.println("Your list: \n");
+//        int allSections = 0;
+//        for (HashMap.Entry<String, ArrayList<Groceries>> pair : list.entrySet()) {
+//            if (!pair.getValue().isEmpty()) {
+//                allSections++;
+//                System.out.printf("%s \n", pair.getKey());
+//                for (Groceries item ; pair.getValue()) {
+//                    if (item.getQuantity() > 1) {
+//                        System.out.printf(" %s, %d, $%.2f each, $%.2f total%n", item.getName(), item.getQuantity(), item.getPrice(), (item.getQuantity() * item.getPrice()));
+//                    } else {
+//                        System.out.printf(" %s. $%.2f%n", item.getName(), item.getPrice());
+//                    }
+//                }
+//                System.out.println();
+//            }
+//        }
+//        if (allSections == 0) {
+//            System.out.println("\nList is empty. \n Please add items to your list");
+//        }
+//    }
+//
+//    public static void runTheApp(HashMap<String, ArrayList<Groceries>> list, Input in) {
+//        System.out.println("Welcome to BT21 Market\n");
+//        System.out.println("*************************");
+//
+//        boolean run = true;
+//        while(run) {
+//            System.out.println("Please make your selection");
+//            System.out.println("1. Add new item to the list");
+//            System.out.println("2. View list");
+//            System.out.println("3. Exit");
 
 //            int userInput = in.getInt("Please enter: 1, 2, or 3", 1, 3);
 //            in.getString();
