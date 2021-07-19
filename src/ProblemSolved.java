8cf0e70d1d3c51b52fe41883784d31345d823f9a
Karen Valadez <valadez.karen04@gmail.com>
2021-07-19 12:15:33 -0500

    commit

--- src/ProblemSolved.java
+++ src/ProblemSolved.java
@@ -1,1 +1,12 @@
+public class ProblemSolved {
+    public static void swapStrings(String [] a, String [] b) {
+        a = new String[]{"apple", "banana", "pear"};
+        b = new String[]{"bob", "sue", "lou"};
 
+        for (int i = 0; i < a.length; i++) {
+            String swap = a[i]; // takes value of bob (index of 0)
+            a[i] = b[i]; // a = {"apple", "sue", "lou"};
+            b[i] = swap ; // b = {"bob", "banana", "pear"};
+        }
+    }
+}
