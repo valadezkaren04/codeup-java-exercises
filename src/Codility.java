f745bf41da819ccd7c155e14590d2cc89972dfdd
Karen Valadez <valadez.karen04@gmail.com>
2021-09-13 11:51:45 -0500

    practice

--- src/Codility.java
+++ src/Codility.java
@@ -1,8 +1,6 @@
 import java.util.Arrays;
-
-
 //    Write a function:
-//
+
 //    class Solution { public int solution(int[] A); }
 //    that, given an array A of N integers, returns the smallest positive integer (greater than 0) that does not occur in A.
 //
