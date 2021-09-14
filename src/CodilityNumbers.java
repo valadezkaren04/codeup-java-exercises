4622dd71113d64c044758c1c78446b256c6e4b7f
Karen Valadez <valadez.karen04@gmail.com>
2021-09-14 12:12:00 -0500

    practice

--- src/CodilityNumbers.java
+++ src/CodilityNumbers.java
@@ -1,2 +1,36 @@
 public class CodilityNumbers {
+    public String solution(String phoneNumber) {
+        phoneNumber = removeNonDigits(phoneNumber);
+        return formatPhoneNumber(phoneNumber, checkNumberSize(phoneNumber));
+    }
+
+    private Boolean checkNumberSize(String phoneNumber) {
+        return phoneNumber.length() % 3 == 1;
+    }
+
+    private String removeNonDigits(String s) {
+        return s.replaceAll("[^0-9]", "");
+    }
+
+    private String formatPhoneNumber(String s, boolean lastGroup) {
+        String tempNumber = "";
+        int dashCounter = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (dashCounter < 3) {
+                tempNumber = tempNumber.concat(s.substring(i, i + 1));
+                dashCounter++;
+            } else if (dashCounter == 3){
+                tempNumber = tempNumber.concat("-");
+                tempNumber = tempNumber.concat(s.substring(i, i + 1));
+                dashCounter = 1;
+            }
+        }
+        if (lastGroup) {
+            char[] temp =  tempNumber.toCharArray();
+            temp[temp.length - 2] = temp[temp.length - 3];
+            temp[temp.length - 3] = '-';
+            tempNumber = new String(temp);
+        }
+        return tempNumber;
+    }
 }
