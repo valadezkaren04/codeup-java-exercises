db24e7b75e5096890d982c8b93896942b16be475
Karen Valadez <valadez.karen04@gmail.com>
2021-09-15 12:12:00 -0500

    practice

--- src/CodilityNonNegativeInt.java
+++ src/CodilityNonNegativeInt.java
@@ -1,2 +1,16 @@
+//Two non-negative integers are called siblings if they can be obtained from each other by rearranging the digits of their decimal representations. For example, 123 and 213 are siblings. 535 and 355 are also siblings.A set consisting of a non-negative integer N and all of its siblings is called the family of N. For example, the family of 553 comprises three numbers: 355, 535 and 553. Write a function that, given a non-negative integer N, returns the largest number in the family of N.
+//
+//        The function should return −1 if the result exceeds 100,000,000. For example, given N = 213 the function should return 321. Given N = 553 the function should return 553.
+//
+//        Write a function
+//
+//class Solutions {
+//
+//    public int solution(int N);
+//
+//}
+//
+//}
+
 public class CodilityNonNegativeInt {
 }
