99ec658c17cd1a19cdfa7a6dbd1ed84a32dc21f1
Karen Valadez <valadez.karen04@gmail.com>
2021-09-16 12:12:00 -0500

    practice

--- src/HelloWorldPartTwo.java
+++ src/HelloWorldPartTwo.java
@@ -1,5 +1,8 @@
 public class HelloWorldPartTwo {
     public static void main(String[] args) {
         System.out.println("Hello World! I'm practicing Java");
+
+        int myFavNum = 14;
+        System.out.println(myFavNum);
     }
 }
