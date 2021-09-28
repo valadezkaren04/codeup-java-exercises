5786c8383cc6820d1f4e10105220582cd776d272
Karen Valadez <valadez.karen04@gmail.com>
2021-09-28 12:12:00 -0500

    practice

--- src/ScannerPractice.java
+++ src/ScannerPractice.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
+
 public class ScannerPractice {
     public static void main(String[] args) {
         double pi = 3.14159;
         System.out.format("The value of pi is approximately %.2f%n", pi);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter an integer.");
+        int userInput = scanner.nextInt();
+        System.out.printf("Your integer was: %d%n", userInput);
     }
 }
